e17310280bb129614a2b806f92fe72c6294b8963
aldoeme <aldomedinamendez1@gmail.com>
2023-08-29 20:22:39 -0600

    Se agrega el proyecto...

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const logo = document.querySelector('.logo_nombre');
+
+
+logo.addEventListener('click',precionarLogo);
+
+function precionarLogo (){
+    alert('Chinga tu madre puto')
+}
+
+const boton = document.getElementsByClassName('boton');
+
+boton.addEventListener('click', () => {
+    alert('Estas seguro de avanzar?')
+} )
